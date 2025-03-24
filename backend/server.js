05995a5dd7a249f0ecca18646818ffb373dfee70
machadeals <machadealsecom@gmail.com>
2025-03-24 20:31:06 +0530

    Updated cors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin:[],
+    methods:["POST","GET","PUT","PATCH","DELETE"],
+    credentials:true
+  }
+));
 app.use(bodyParser.json());
 
 // Routes
