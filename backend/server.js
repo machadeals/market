f669cfb849eb8f407bb79d2709979af83fd6e450
machadeals <machadealsecom@gmail.com>
2025-03-24 22:58:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://market-frontend-lovat.vercel.app", // Allow frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  credentials: true  // Allow cookies, authentication headers
+}));
 app.use(bodyParser.json());
 
 // Routes
