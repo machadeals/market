09fa368b0e3b47d574f52a2af22164ba289d992d
Lokesh <dontulalokesh@gmail.com>
2025-03-25 12:53:50 +0530

    modified cors

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: "https://market-frontend-lovat.vercel.app", // Allow frontend URL
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  credentials: true  // Allow cookies, authentication headers
-}));
+app.use(
+  cors({
+    origin: process.env.FRONTURL, // Allow frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true, // Allow cookies, authentication headers
+  })
+);
 app.use(bodyParser.json());
 
 // Routes
